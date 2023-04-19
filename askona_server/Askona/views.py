426a32e0148dc878c2dcb97c5f8aeb72372b1569
nickita <nickita.seomin@yandex.ru>
2023-04-19 21:32:03 +0300

    Add MattressViewSet

--- askona_server/Askona/views.py
+++ askona_server/Askona/views.py
@@ -11,30 +11,9 @@ from .models import Mattress, Pillow
 from .serializers import MattressSerializer, MattressSerializerImage, PillowSerializer
 
 
-class ImageUploadView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Mattress.objects.all()
-    serializer_class = MattressSerializerImage
-
-
-class MattressAPIList(generics.ListAPIView):
+class MattressViewSet(viewsets.ModelViewSet):
     queryset = Mattress.objects.all()
     serializer_class = MattressSerializer
-
-
-class MattressAPICreate(generics.CreateAPIView):
-    queryset = Mattress.objects.all()
-    serializer_class = MattressSerializer
-
-
-class MattressAPIUpdate(generics.UpdateAPIView):
-    queryset = Mattress.objects.all()
-    serializer_class = MattressSerializer
-
-
-class MattressAPICRUD(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Mattress.objects.all()
-    serializer_class = MattressSerializer
-
 
 
 class PillowAPIList(generics.ListAPIView):
--- askona_server/askona_server/urls.py
+++ askona_server/askona_server/urls.py
@@ -4,9 +4,9 @@ from Askona.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/mattresslist/', MattressAPIList.as_view()),
-    path('api/v1/mattresslist/', MattressAPICreate.as_view()),
-    path('api/v1/mattresslist/<int:pk>/', MattressAPIUpdate.as_view()),
+    path('api/v1/mattresslist/', MattressViewSet.as_view({'get': 'list','post':'create'})),
+    path('api/v1/mattresslist/<int:pk>/', MattressViewSet.as_view({'put': 'update', 'delete':'destroy'})),
+
     path('api/v1/pillowlist/', PillowAPIList.as_view()),
-    path('api/v1/pillowlist/<int:pk>/', PillowAPICRUD.as_view()),
+    path('api/v1/pillowlist/<int:pk>/', PillowAPICRUD.as_view())
 ]
