d521ecd816b54317747606cfccc8b2f9c0256af3
nickita <nickita.seomin@yandex.ru>
2023-04-26 00:16:28 +0300

    return photo v1

--- askona_server/Askona/serializers.py
+++ askona_server/Askona/serializers.py
@@ -1,5 +1,3 @@
-from abc import ABC
-
 from rest_framework import serializers
 from .models import Mattress, Pillow
 
@@ -8,6 +6,23 @@ class MattressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mattress
         fields = ('title', 'min_height', 'max_height', 'min_weight', 'max_weight', 'photo')
+
+    def get_photo_url(self, obj):
+        request = self.context.get('request')
+        photo_url = obj.fingerprint.url
+        return request.build_absolute_uri(photo_url)
+
+
+
+class MattressPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mattress
+        fields = ('photo',)
+
+    def get_photo_url(self, obj):
+        request = self.context.get('request')
+        photo_url = obj.fingerprint.url
+        return request.build_absolute_uri(photo_url)
 
 
 class PillowSerializer(serializers.ModelSerializer):
--- askona_server/Askona/views.py
+++ askona_server/Askona/views.py
@@ -1,18 +1,66 @@
-from rest_framework import generics, viewsets
-
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
+from rest_framework import generics, viewsets, serializers
+from rest_framework.decorators import renderer_classes, api_view
+from rest_framework.generics import ListAPIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.utils import json
+from rest_framework.views import APIView
 
 from .models import Mattress, Pillow
-from .serializers import MattressSerializer, PillowSerializer
+from .renders import JPEGRenderer, PNGRenderer
+from .serializers import MattressSerializer, PillowSerializer, MattressPhotoSerializer
 
 
 class MattressViewSet(viewsets.ModelViewSet):
     queryset = Mattress.objects.all()
     serializer_class = MattressSerializer
 
+    def get(self, request, format=None):
+        queryset = Mattress.objects.all()
+        serializer = serializers.MattressSerializer(queryset, context={"request": request}, many=True)
+        return Response(serializer.data)
+
+
+class MattressAPICRUD(generics.RetrieveUpdateDestroyAPIView):
+    # renderer_classes = [JPEGRenderer, PNGRenderer]
+
+    queryset = Mattress.objects.all()
+    serializer_class = MattressPhotoSerializer
+#
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(FileWrapper(Mattress.objects.get(id=serializer.data['id'])['image'].open()))
+#             # You can also specify the content_type in your response
+#         return Response(data={"detail": "Serializer Error"})
+
+# @api_view(['GET'])
+
+# # @detail_route(methods=['get'], renderer_classes=(PDFRenderer,))
+# class MattressAPICRUD(APIView):
+#
+#     @renderer_classes([JSONRenderer])
+#     def get(self, request, *args, **kwargs):
+#         with open('../images/2021-11-30.png', 'rb') as report:
+#             return Response(
+#                 report.read(),
+#                 headers={'Content-Disposition': 'attachment; filename=2021-11-30.png'},
+#                 content_type='application/png')
+    # @renderer_classes([JSONRenderer])
+
+    # def get(self, request, pk):
+    #     m = Mattress.objects.all()
+    #     return HttpResponse(content='<img src="{{MattressPhotoSerializer(m).data.get(\'photo\')}}" />')
+    #     # return Response(MattressPhotoSerializer(m).data.get('photo') , content_type='image/png')
+
 
 class PillowAPIList(generics.ListAPIView):
     queryset = Pillow.objects.all()
     serializer_class = PillowSerializer
+
 
 
 class PillowAPIUpdate(generics.UpdateAPIView):
