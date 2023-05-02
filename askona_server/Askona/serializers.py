ab7d092c4f473783589a5e6babe4b9e19aafb1d2
nickita <nickita.seomin@yandex.ru>
2023-05-02 21:36:48 +0300

    add serializer user

--- askona_server/Askona/serializers.py
+++ askona_server/Askona/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
-from .models import Mattress, Pillow
+from .models import Mattress, Pillow, Users
+
+
+# ----------------------users------------------------------------
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('name', 'surname', 'second_name', 'userPhoto', 'userConturPhoto')
+
+    def get_photo_url(self, obj):
+        request = self.context.get('request')
+        photo_url = obj.fingerprint.url
+        return request.build_absolute_uri(photo_url)
+#----------------------------------------------------------------------
 
 
 class MattressSerializer(serializers.ModelSerializer):
